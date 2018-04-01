4a808958494d941557e699f371f3edc77867d974
Caleb Cartwright <caleb.cartwright@outlook.com>
2018-03-31 23:20:31 -0500

    seeing if a js file will work for the module

---
+++ base/index.js
@@ -0,0 +1,12 @@
+module.exports = {
+    "reporter": [
+        "html",
+        "lcov",
+        "cobertura",
+        "text",
+        "text-summary"
+    ],
+    "cache": true,
+    "all": true,
+    "per-file": true
+}
