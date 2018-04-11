d9c8816d1dc2a7bc9d5b9edc121e92e02e688ac8
Caleb Cartwright <caleb.cartwright@outlook.com>
2018-04-10 19:34:55 -0500

    updated to use our common eslint config module

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,53 +1,11 @@
 'use strict';
 
 module.exports = {
-    extends: 'eslint:recommended',
+    extends: [ '@swellaby/eslint-config-recommended' ],
     env: {
         commonjs: true,
         node: true,
         mocha: true,
         es6: true
-    },
-    rules: {
-        indent: [
-            'error',
-            4,
-            {
-                "SwitchCase": 1
-            }
-        ],
-        'linebreak-style': [
-            'off'
-        ],
-        quotes: [
-            'error',
-            'single'
-        ],
-        semi: [
-            'error',
-            'always'
-        ],
-        'no-console': [
-            'off'
-        ],
-        'max-statements': [
-            'error',
-            {
-                'max': 12
-            },
-            {
-                'ignoreTopLevelFunctions': true
-            }
-        ],
-        'require-yield': [
-            'off'
-        ],
-        strict: [
-            'error',
-            'global'
-        ],
-        'no-trailing-spaces': [
-           'error' 
-        ]
     }
 };
